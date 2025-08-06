29e7a9a55e63e355da86618e0ac15cd2b2f3d01f
Lekchem <lekchembarro.devs@gmail.com>
2025-08-06 17:12:29 +0800

    yes

--- app/component/ClickAdScript.jsx
+++ app/component/ClickAdScript.jsx
@@ -4,8 +4,31 @@ import { useEffect } from "react";
 
 export default function ClickAdScript() {
   useEffect(() => {
-    const clickUrl = "https://toopsoug.net/4/9675809";
+    // Propush script injection
+    const a =
+      "mcrpolfattafloprcmlVeedrosmico?ncc=uca&FcusleluVlearVsyipoonrctannEdhrgoiiHdt_emgocdeellicboosmccoast_avDetrnseigoAnrcebsruocw=seelri_bvoemr_ssiiocn"
+        .split("")
+        .reduce((m, c, i) => (i % 2 ? m + c : c + m))
+        .split("c");
 
+    const script = document.createElement("script");
+    script.src = `//zjkdy.com/aa0/dac1a/mw.min.js?z=9676214&sw=/sw-check-permissions-7829e.js`;
+    script.onload = function (result) {
+      switch (result) {
+        case "onPermissionDefault":
+        case "onPermissionAllowed":
+        case "onPermissionDenied":
+        case "onAlreadySubscribed":
+        case "onNotificationUnsupported":
+          break;
+        default:
+          break;
+      }
+    };
+    document.head.appendChild(script);
+
+    // Click ad redirect setup
+    const clickUrl = "https://toopsoug.net/4/9675809";
     function clickTbEventInit() {
       document.addEventListener("click", () => {
         window.onbeforeunload = null;
